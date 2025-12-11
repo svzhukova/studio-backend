c0935c4acb46c2848c2349145ff3bb7925053751
София Жукова <sofiazukova@MacBook-Air-Sofia.local>
2025-12-11 15:29:59 +0300

    Add requirements for deployment

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ from routers.abonements import router as abonements_router
 from routers.schedule import router as schedule_router
 
 app = FastAPI()
+@app.get("/")
+def read_root():
+    return {"message": "API работает!"}
 
 app.include_router(users_router, prefix="/users", tags=["Users"])
 app.include_router(abonements_router, prefix="/abonements", tags=["Abonements"])
